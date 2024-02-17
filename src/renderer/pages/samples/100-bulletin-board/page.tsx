4b661016a91a8bee105b8376c534ec4c173e5561
atman <gpbjk0304@gmail.com>
2024-02-17 18:38:01 +0900

    add font awesome

--- src/renderer/pages/samples/100-bulletin-board/page.tsx
+++ src/renderer/pages/samples/100-bulletin-board/page.tsx
@@ -1,9 +1,12 @@
+import { faPen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SampleLayout from '../_components/layout';
 
 const BulleinBoardPage = () => {
   return (
     <SampleLayout>
       <div>BulleinBoardPage</div>
+      <FontAwesomeIcon icon={faPen} className="mr-2 h-6 w-6" />
     </SampleLayout>
   );
 };
